b2699d8dfcd570df508f08304d4e595c42d1de64
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-11-23 14:51:14 +0530

    change router path

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: "static",
   router: {
-    base: '/app/'
+    base: 'astronomia-site'
   },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
