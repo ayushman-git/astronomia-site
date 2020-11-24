18dc81cc9f672af72b7230e3b3987ea4cee99f85
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-11-24 14:34:57 +0530

    fixed config

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,44 +6,44 @@ export default {
   publicRuntimeConfig: {
     googleAnalytics: {
       id: "G-7Q1F03FQX3"
-    },
-    target: "static",
-    router: {
-      base: "/astronomia-site/"
-    },
-    // Global page headers (https://go.nuxtjs.dev/config-head)
-    head: {
-      title: "Astronomia",
-      meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { hid: "description", name: "description", content: "" }
-      ],
-      link: [
-        {
-          rel: "icon",
-          type: "image/x-icon",
-          href: "/astronomia-site/favicon.ico"
-        }
-      ]
-    },
+    }
+  },
+  target: "static",
+  router: {
+    base: "/astronomia-site/"
+  },
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: "Astronomia",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
+    ],
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/astronomia-site/favicon.ico"
+      }
+    ]
+  },
 
-    // Global CSS (https://go.nuxtjs.dev/config-css)
-    css: [],
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [],
 
-    // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-    plugins: [],
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
 
-    // Auto import components (https://go.nuxtjs.dev/config-components)
-    components: true,
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
 
-    // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-    buildModules: ["@nuxtjs/google-analytics"],
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: ["@nuxtjs/google-analytics"],
 
-    // Modules (https://go.nuxtjs.dev/config-modules)
-    modules: [],
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [],
 
-    // Build Configuration (https://go.nuxtjs.dev/config-build)
-    build: {}
-  }
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {}
 };
